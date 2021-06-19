195217c06e85c5df475effc1facb8c52ab3e2cdf
avinash <avszoom@gmail.com>
2021-06-19 11:20:42 +0530

    first commit

---
+++ src/main/java/com/avszoom/in/microservicesecurity/MicroserviceSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.avszoom.in.microservicesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSecurityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/avszoom/in/microservicesecurity/resources/HomeResource.java
@@ -0,0 +1,13 @@
+package com.avszoom.in.microservicesecurity.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String getHomePage(){
+        return "<h1>Hello</h1>";
+    }
+}
